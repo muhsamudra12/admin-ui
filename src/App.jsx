cdb04263b85b1d3f10a800b458215ff2c9d84f63
muhsamudra12 <samudraiya@gmail.com>
2025-10-12 00:19:07 +0700

    menggunakan variant class tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,125 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div className="min-h-screen flex flex-col items-center justify-center text-center bg-lime-400">
-        <div className="flex items-center justify-center space-x-8 mb-12">
-          <a href="https://vite.dev" target="_blank">
-            <img
-              src={viteLogo}
-              className="w-24 h-24 animate-spin"
-              alt="Vite logo"
-              style={{ animationDuration: "5s" }}
-            />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img
-              src={reactLogo}
-              className="w-24 h-24 animate-spin"
-              alt="React logo"
-              style={{ animationDuration: "5s" }}
-            />
-          </a>
+      <div className="p-6 space-y-8">
+        {/* LATIHAN 1 - NAVBAR */}
+        <h2 className="mb-2">LATIHAN 1</h2>
+        <nav className="flex flex-col md:flex-row justify-between  p-4 bg-gray-100 rounded">
+          {/* Kiri - Logo */}
+          <div className="font-bold text-lg mb-2 md:mb-0">Logo Saya</div>
+
+          {/* Kanan - Menu */}
+          <ul className="flex flex-col md:flex-row gap-2 md:gap-6 text-gray-700">
+            <li>
+              <a href="#" className="hover:underline">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        <hr />
+
+        {/* LATIHAN 2 - GRID GALERI */}
+        <h2 className="mb-2">LATIHAN 2</h2>
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+          <img
+            src="https://www.placehold.co/200x200"
+            alt="Gambar Galeri"
+            className="h-48"
+          />
+          <img
+            src="https://www.placehold.co/200x200"
+            alt="Gambar Galeri"
+            className="h-48"
+          />
+          <img
+            src="https://www.placehold.co/200x200"
+            alt="Gambar Galeri"
+            className="h-48"
+          />
+          <img
+            src="https://www.placehold.co/200x200"
+            alt="Gambar Galeri"
+            className="h-48"
+          />
+          <img
+            src="https://www.placehold.co/200x200"
+            alt="Gambar Galeri"
+            className="h-48"
+          />
+          <img
+            src="https://www.placehold.co/200x200"
+            alt="Gambar Galeri"
+            className="h-48"
+          />
         </div>
 
-        <h1 className="text-blue-400 text-6xl  font-bold mb-16">
-          Vite + React
-        </h1>
-        <div className=" text-sm space-y-6 mb-8">
-          <button
-            onClick={() => setCount((count) => count + 1)}
-            className="bg-lime-100 text-neutral py-2 px-6 rounded-lg text-lg"
-          >
-            angka ke piro brok {count}
-          </button>
-          <p className="text-white">
-            kanggo edit <code>src/App.jsx</code> dan simpan dan keluar
-          </p>
+        <hr />
+
+        {/* LATIHAN 3 - PRICING TABLE */}
+        <h2 className="mb-2">LATIHAN 3</h2>
+        <div className="grid md:grid-cols-3 gap-6 text-center p-4">
+          <div className="bg-gray-200 p-6 rounded shadow">
+            <h3>Basic</h3>
+            <p>Rp 50.000</p>
+          </div>
+          <div className="bg-blue-200 p-8 rounded shadow-md scale-105">
+            <h3>Pro</h3>
+            <p>Rp 100.000</p>
+          </div>
+          <div className="bg-gray-200 p-6 rounded shadow">
+            <h3>Premium</h3>
+            <p>Rp 200.000</p>
+          </div>
         </div>
-        <p className="text-neutral-800">aduhai,mantep brok ya ga brok</p>
+
+        <hr />
+
+        {/* LATIHAN 4 - DASHBOARD LAYOUT */}
+        <h2 className="mb-2">LATIHAN 4</h2>
+        <div className="grid grid-cols-1 md:grid-cols-12 min-h-[100vh] gap-2">
+          <header className="md:col-span-12 bg-gray-300 p-4 rounded">
+            Header
+          </header>
+          <aside className="md:col-span-3 bg-gray-200 p-4 rounded">
+            Sidebar
+          </aside>
+          <main className="md:col-span-9 bg-white p-4 border rounded">
+            Content
+          </main>
+          <footer className="md:col-span-12 bg-gray-300 p-4 rounded">
+            Footer
+          </footer>
+        </div>
+
+        <hr />
+
+        {/* LATIHAN 5 - CARD PRODUCT */}
+        <h2 className="mb-1">LATIHAN 5</h2>
+        <div className="flex flex-col items-center text-center p-4 border rounded shadow md:flex-row md:items-start md:text-left md:gap-4">
+          <img
+            src="https://www.placehold.co/80x80"
+            alt="Gambar Produk"
+            className="rounded-md"
+          />
+          <div className="flex-col p-4">
+            <h3 className="font-bold text-lg">Nama Produk</h3>
+            <p className="text-sm text-gray-600">Deskripsi produk singkat...</p>
+          </div>
+        </div>
       </div>
     </>
   );
