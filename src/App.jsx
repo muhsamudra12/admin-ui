645a0fe54c93003d0d2a1861ae90d329cc83e28b
muhsamudra12 <samudraiya@gmail.com>
2025-10-14 09:54:25 +0700

    membuat page sign in

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
         <div className="flex justify-center font-poppins tracking-wide text-primary text-4xl">
           <span className="font-bold">FINE</span>
           bank
-          <span className="font-bold">.IO</span>
+          <span className="font-bold">.IO)</span>
         </div>
         {/* logo end */}
         {/* form start */}
